1b49184b56faa1a99a8fa4515afef625aaaab382
DovCaio <onlydeadnotalive@gmail.com>
2025-11-29 14:38:09 -0300

    rafact: cria os dto's de lot, create e put

--- src/repository/lot.repository.ts
+++ src/repository/lot.repository.ts
@@ -1,14 +1,16 @@
+import { LotCreateDTO } from "../dto/ lot/LotCreateDTO";
+import { LotPutDTO } from "../dto/ lot/LotPutDTO";
 import prisma from "../prisma";
 
 export const LotRepository = {
 
-    async create(data: any) {
+    async create(data: LotCreateDTO) {
 
         return prisma.lot.create({
             data
         });
     },
-    async update(id: number, data: any) { 
+    async update(id: number, data: LotPutDTO) { 
         return prisma.lot.update({
             where: { id },
             data
--- src/service/lot.service.ts
+++ src/service/lot.service.ts
@@ -1,11 +1,13 @@
 import { LotRepository } from "../repository/lot.repository";
+import { LotCreateDTO } from "../dto/ lot/LotCreateDTO";
+import { LotPutDTO } from "../dto/ lot/LotPutDTO";
 
 export const LotService = {
-    async create(data: any) {
+    async create(data: LotCreateDTO) {
         const lot = await LotRepository.create(data);
         return lot;
     },
-    async update(id: number, data: any) {
+    async update(id: number, data: LotPutDTO) {
         const lot = await LotRepository.update(id, data);
         return lot;
     }
