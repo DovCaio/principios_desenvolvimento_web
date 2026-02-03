559c16f7b9b48a3a56464d1dd120e0fa02c8bba5
crissalves <cristianalves_cg@hotmail.com>
2026-02-02 22:21:37 -0300

    Autenticacao inicial

---
+++ src/service/auth.service.ts
@@ -0,0 +1,32 @@
+import { UserType } from "@prisma/client";
+import { hash } from "bcryptjs";
+import { EmployeeCreateDTO } from "../dto/employee/EmployeeCreateDTO";
+import { UserPutDTO } from "../dto/user/UserPutDTO";
+import { EmployeeRepository } from "../repository/employee.repository";
+
+export const EmployeeService = {
+
+    async create(userDto: EmployeeCreateDTO) {
+        const hashedPassword = await hash(userDto.password, 8);
+        
+        const dataWithHash = {
+            ...userDto,
+            password: hashedPassword,
+            userType: UserType.EMPLOYEE 
+        };
+
+        return EmployeeRepository.createEmployee(dataWithHash as any);
+    },
+
+    async update(cpf: string, userDto: UserPutDTO) {
+        return EmployeeRepository.updateEmployee(cpf, userDto);
+    },
+
+    async get(cpf: string) {
+        return EmployeeRepository.getEmployee(cpf);
+    },
+
+    async delete(cpf: string) {
+        return EmployeeRepository.deleteEmployee(cpf);
+    }
+}
